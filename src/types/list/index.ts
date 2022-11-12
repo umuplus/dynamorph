4b652d23417d37737671c5ab76316898d7ee4b7d
Umut Aydin <aydinumut82@gmail.com>
2022-11-12 19:08:47 +0300

    list type in progress tests added for list type

---
+++ src/types/list/index.ts
@@ -0,0 +1,68 @@
+import { Attribute, BaseType, KeyType } from '../../utils/types'
+import { Exception } from '../../utils/errors'
+import { silent } from '../../utils/helpers'
+
+type ListBaseType = Omit<Attribute, 'type'>
+
+export interface ListOptions extends ListBaseType {
+    min?: number
+    max?: number
+    length?: number
+    validate?: (v: any[] | undefined) => string | undefined
+    transform?: (v: any[] | undefined) => any[] | undefined
+    default?: () => any[]
+}
+
+export class ListType extends BaseType {
+    protected _value: any[] | undefined = undefined
+
+    constructor(options: ListOptions) {
+        super({ ...options, type: KeyType.NUMBER })
+    }
+
+    protected parse(v: any[] | undefined): Exception | void {
+        const error = new Exception()
+        const type = typeof v
+        const { min, max, length, validate } = this._options
+        if (type === 'undefined') {
+            if (this._options.required) error.addIssue({ path: 'value', expected: 'array', received: type })
+        } else if (Array.isArray(v)) {
+            if (min !== undefined && v!.length < min) error.addIssue({ path: 'length', expected: `${min}<=`, received: v?.length })
+            if (max !== undefined && v!.length > max) error.addIssue({ path: 'length', expected: `<=${max}`, received: v?.length })
+            if (length !== undefined && v!.length !== length) error.addIssue({ path: 'length', expected: length, received: v?.length })
+
+            if (validate) {
+                const message = validate(v)
+                if (message) error.addIssue({ path: 'value', message })
+            }
+        } else error.addIssue({ path: 'value', expected: 'array', received: type })
+        if (error.hasIssues()) return error
+    }
+
+    set value(v: any[] | undefined) {
+        const error = this.parse(v)
+        if (error) {
+            if (!silent()) throw error
+
+            if (this.error) this.error.addIssues(error.issues)
+            else this._error = error
+        } else {
+            if (this._options.transform) v = this._options.transform(v)
+            if (this._options.default && v === undefined) v = this._options.default()
+            if (this._value !== v) {
+                this._changed = true
+                this._value = v
+            }
+        }
+    }
+
+    get value(): any[] | undefined {
+        return this._value
+    }
+
+    get changed(): boolean {
+        return this._changed
+    }
+}
+
+export default ListType
---
+++ src/types/list/list.test.ts
@@ -0,0 +1,110 @@
+import ListType from '.'
+import test from 'ava'
+import { silent } from '../../utils/helpers'
+import { Exception } from '../../utils/errors'
+
+test.before(() => silent(true))
+
+test('a simple required list attribute', (t) => {
+    const attribute = new ListType({ fieldName: 'items', min: 1, max: 10, length: 3, required: true })
+    attribute.value = ['a', 'b', 'c']
+    t.is(attribute.error, undefined)
+    t.is(attribute.fieldName, 'items')
+    t.deepEqual(attribute.value, ['a', 'b', 'c'])
+    t.is(attribute.changed, true)
+})
+
+test('a simple list attribute', (t) => {
+    const attribute = new ListType({ min: 1, max: 10, length: 3 })
+    attribute.value = undefined
+    t.is(attribute.error, undefined)
+    t.is(attribute.value, undefined)
+    t.is(attribute.changed, false)
+})
+
+test('a simple list attribute with default', (t) => {
+    const attribute = new ListType({ default: () => ['a', 'b', 'c'] })
+    attribute.value = undefined
+    t.is(attribute.error, undefined)
+    t.is(JSON.stringify(attribute.value), '["a","b","c"]')
+    t.is(attribute.changed, true)
+})
+
+test('input converted to list via transform', (t) => {
+    // TODO? transform first, then validate maybe?
+    const attribute = new ListType({ transform: (v) => v?.map((k) => k?.toUpperCase() || k) })
+    attribute.value = ['a', 'b', 'c']
+    t.is(attribute.error, undefined)
+    t.deepEqual(attribute.value, ['A', 'B', 'C'])
+    t.is(attribute.changed, true)
+})
+
+test('custom validator fails for a list attribute', (t) => {
+    const attribute = new ListType({ validate: (v) => ((v?.length || 0) % 2 ? 'number of items in the value must be even' : undefined) })
+    attribute.value = ['a', 'b', 'c']
+    t.is(attribute.error instanceof Exception, true)
+    t.deepEqual(attribute.error?.issues, [
+        {
+            message: 'number of items in the value must be even',
+            path: 'value',
+        },
+    ])
+    t.is(!!attribute.value, false)
+})
+
+test('cannot assign object to a list attribute', (t) => {
+    const attribute = new ListType({})
+    attribute.value = JSON.parse('{"a":1}')
+    t.deepEqual(attribute.error?.issues, [
+        {
+            expected: 'array',
+            message: '"value" is expected to be "array" but received "object"',
+            path: 'value',
+            received: 'object',
+        },
+    ])
+    t.is(!!attribute.value, false)
+})
+
+test('cannot assign above of allowed range value to list attribute', (t) => {
+    const attribute = new ListType({ max: 2 })
+    attribute.value = ['a', 'b', 'c', 'd']
+    t.deepEqual(attribute.error?.issues, [
+        {
+            expected: '<=2',
+            message: '"length" is expected to be "<=2" but received "4"',
+            path: 'length',
+            received: 4,
+        },
+    ])
+    t.is(!!attribute.value, false)
+})
+
+test('cannot assign below of allowed range value to list attribute', (t) => {
+    const attribute = new ListType({ min: 3 })
+    attribute.value = ['a']
+    t.deepEqual(attribute.error?.issues, [
+        {
+            expected: '3<=',
+            message: '"length" is expected to be "3<=" but received "1"',
+            path: 'length',
+            received: 1,
+        },
+    ])
+    t.is(!!attribute.value, false)
+})
+
+test('cannot assign undefined to required list attribute', (t) => {
+    const attribute = new ListType({ required: true })
+    attribute.value = undefined
+    t.is(attribute.error instanceof Exception, true)
+    t.deepEqual(attribute.error?.issues, [
+        {
+            expected: 'array',
+            message: '"value" is expected to be "array" but received "undefined"',
+            path: 'value',
+            received: 'undefined',
+        },
+    ])
+    t.is(attribute.changed, false)
+})
