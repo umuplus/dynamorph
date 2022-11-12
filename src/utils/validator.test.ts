8e3c2e4899afdec292659c4913930ac1ce6d2848
Umut Aydin <aydinumut82@gmail.com>
2022-11-12 14:19:52 +0300

    simple validators and their tests added

--- src/utils/validator.test.ts
+++ src/utils/validator.test.ts
@@ -1,9 +1,22 @@
 import test from 'ava'
-import { isUlid } from './validator'
+import { isEmail, isUlid, isUrl } from './validator'
 import { ulid } from 'ulid'
+
+test('isEmail', (t) => {
+    t.is(isEmail('info@example.com'), true)
+    t.is(isEmail('test'), false)
+})
 
 test('isUlid', (t) => {
     t.is(isUlid(Math.random()), false)
     t.is(isUlid(Math.random().toString()), false)
     t.is(isUlid(ulid()), true)
+})
+
+test('isUrl', (t) => {
+    t.is(isUrl('https://example.com'), true)
+    t.is(isUrl('https://example.com/some-path'), true)
+    t.is(isUrl('example.com/some-path'), true)
+    t.is(isUrl('example-com/some-path'), false)
+    t.is(isUrl('{example.com}_[some-path]'), false)
 })
--- src/utils/validator.ts
+++ src/utils/validator.ts
@@ -1,4 +1,14 @@
-const pattern = /[0-9A-HJKMNP-TV-Z]{26}/
-export function isUlid(value: any): boolean {
-    return typeof value === 'string' && pattern.test(value)
+const ulidPattern = /^[0-9A-HJ-NP-TV-Z]{26}$/
+export function isUlid(value?: any): boolean {
+    return typeof value === 'string' && ulidPattern.test(value)
+}
+
+const emailPattern = /^[^\s@]+@[^\s@]+$/
+export function isEmail(value?: any): boolean {
+    return typeof value === 'string' && emailPattern.test(value)
+}
+
+const urlPattern = /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w .-]*)*\/?$/
+export function isUrl(value?: any): boolean {
+    return typeof value === 'string' && urlPattern.test(value)
 }
