9d6abc28a8cbeb4d2baa26f0bd6d9e56f654643a
Umut Aydin <aydinumut82@gmail.com>
2022-11-12 10:23:49 +0300

    test coverage improved for boolean type

--- src/types/boolean/boolean.test.ts
+++ src/types/boolean/boolean.test.ts
@@ -6,11 +6,12 @@ import { Exception } from '../../utils/errors'
 test.before(() => silent(true))
 
 test('a simple required boolean attribute', (t) => {
-    const attribute = new BooleanType({ required: true })
+    const attribute = new BooleanType({ fieldName: 'isOK', required: true })
     attribute.value = true
     t.is(attribute.error, undefined)
     t.is(attribute.value, true)
     t.is(attribute.changed, true)
+    t.is(attribute.fieldName, 'isOK')
 })
 
 test('a simple boolean attribute', (t) => {
@@ -29,6 +30,21 @@ test('input converted to boolean via transform', (t) => {
     t.is(attribute.changed, true)
 })
 
+test('cannot assign undefined to a required boolean attribute', (t) => {
+    const attribute = new BooleanType({ required: true })
+    attribute.value = undefined
+    t.is(attribute.error instanceof Exception, true)
+    t.deepEqual(attribute.error?.issues, [
+        {
+            expected: 'boolean',
+            message: '"value" is expected to be "boolean" but received "undefined"',
+            path: 'value',
+            received: 'undefined',
+        },
+    ])
+    t.is(!!attribute.value, false)
+})
+
 test('cannot assign an object to boolean attribute', (t) => {
     const attribute = new BooleanType({ required: true })
     attribute.value = JSON.parse('{"a":1}')
@@ -39,6 +55,40 @@ test('cannot assign an object to boolean attribute', (t) => {
             message: '"value" is expected to be "boolean" but received "object"',
             path: 'value',
             received: 'object',
+        },
+    ])
+    t.is(!!attribute.value, false)
+})
+
+test('a boolean attribute cannot be marked as partition key or sort key', (t) => {
+    const attribute = new BooleanType({ partitionKey: true, sortKey: true })
+    attribute.value = true
+    t.is(attribute.error instanceof Exception, true)
+    t.deepEqual(attribute.error?.issues, [
+        {
+            expected: 'string|number',
+            message: 'Partition key cannot be a boolean',
+            path: 'PartitionKey',
+            received: 'boolean',
+        },
+        {
+            expected: 'string|number',
+            message: 'Sort key cannot be a boolean',
+            path: 'SortKey',
+            received: 'boolean',
+        },
+    ])
+    t.is(!!attribute.value, false)
+})
+
+test('custom validator fails for a boolean attribute', (t) => {
+    const attribute = new BooleanType({ validate: (v) => (!v ? 'value must be true' : undefined) })
+    attribute.value = false
+    t.is(attribute.error instanceof Exception, true)
+    t.deepEqual(attribute.error?.issues, [
+        {
+            message: 'value must be true',
+            path: 'value',
         },
     ])
     t.is(!!attribute.value, false)
