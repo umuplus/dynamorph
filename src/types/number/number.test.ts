c0d8ca459119f708fa6fb96310d04fbb8c4b831d
Umut Aydin <aydinumut82@gmail.com>
2022-11-12 13:52:20 +0300

    test coverage improved for number type

--- src/types/number/number.test.ts
+++ src/types/number/number.test.ts
@@ -6,9 +6,10 @@ import { Exception } from '../../utils/errors'
 test.before(() => silent(true))
 
 test('a simple required number attribute', (t) => {
-    const attribute = new NumberType({ gte: 1, lte: 10, required: true })
+    const attribute = new NumberType({ fieldName: 'age', gte: 1, lte: 10, required: true })
     attribute.value = 5
     t.is(attribute.error, undefined)
+    t.is(attribute.fieldName, 'age')
     t.is(attribute.value, 5)
     t.is(attribute.changed, true)
 })
@@ -30,8 +31,36 @@ test('a simple number attribute', (t) => {
     t.is(attribute.changed, false)
 })
 
-test('cannot assign out of range value to number attribute', (t) => {
+test('custom validator fails for a number attribute', (t) => {
+    const attribute = new NumberType({ validate: (v) => (v! % 2 ? 'value must be even' : undefined) })
+    attribute.value = 3
+    t.is(attribute.error instanceof Exception, true)
+    t.deepEqual(attribute.error?.issues, [
+        {
+            message: 'value must be even',
+            path: 'value',
+        },
+    ])
+    t.is(!!attribute.value, false)
+})
+
+
+test('cannot assign object to a number attribute', (t) => {
     const attribute = new NumberType({ gte: 1, lte: 10 })
+    attribute.value = JSON.parse('{"a":1}')
+    t.deepEqual(attribute.error?.issues, [
+        {
+            expected: 'number',
+            message: '"value" is expected to be "number" but received "object"',
+            path: 'value',
+            received: 'object',
+        },
+    ])
+    t.is(!!attribute.value, false)
+})
+
+test('cannot assign above of allowed range value to number attribute', (t) => {
+    const attribute = new NumberType({ gte: 5, lte: 10 })
     attribute.value = 15
     t.deepEqual(attribute.error?.issues, [
         {
@@ -39,6 +68,20 @@ test('cannot assign out of range value to number attribute', (t) => {
             message: '"value" is expected to be "<=10" but received "15"',
             path: 'value',
             received: 15,
+        },
+    ])
+    t.is(!!attribute.value, false)
+})
+
+test('cannot assign below of allowed range value to number attribute', (t) => {
+    const attribute = new NumberType({ gte: 5, lte: 10 })
+    attribute.value = 1
+    t.deepEqual(attribute.error?.issues, [
+        {
+            expected: '>=5',
+            message: '"value" is expected to be ">=5" but received "1"',
+            path: 'value',
+            received: 1,
         },
     ])
     t.is(!!attribute.value, false)
@@ -54,6 +97,36 @@ test('cannot assign undefined to required number attribute', (t) => {
             message: '"value" is expected to be "number" but received "undefined"',
             path: 'value',
             received: 'undefined',
+        },
+    ])
+    t.is(attribute.changed, false)
+})
+
+test('cannot assign float to integer attribute', (t) => {
+    const attribute = new NumberType({ int: true })
+    attribute.value = 1.5
+    t.is(attribute.error instanceof Exception, true)
+    t.deepEqual(attribute.error?.issues, [
+        {
+            expected: 'integer',
+            message: '"value" is expected to be "integer" but received "float"',
+            path: 'value',
+            received: 'float',
+        },
+    ])
+    t.is(attribute.changed, false)
+})
+
+test('cannot assign integer to float attribute', (t) => {
+    const attribute = new NumberType({ float: true })
+    attribute.value = 1
+    t.is(attribute.error instanceof Exception, true)
+    t.deepEqual(attribute.error?.issues, [
+        {
+            expected: 'float',
+            message: '"value" is expected to be "float" but received "integer"',
+            path: 'value',
+            received: 'integer',
         },
     ])
     t.is(attribute.changed, false)
