c2757bc19c88f8e5d830378c23be825c6334bb91
Umut Aydin <aydinumut82@gmail.com>
2022-11-12 13:52:07 +0300

    custom validator added to number type

--- src/types/number/index.ts
+++ src/types/number/index.ts
@@ -12,6 +12,7 @@ export interface NumberOptions extends NumberBaseType {
     gte?: number
     float?: boolean
     int?: boolean
+    validate?: (v: number | undefined) => string | undefined
     transform?: (v: number | undefined) => number | undefined
 }
 
@@ -25,16 +26,22 @@ export class NumberType extends BaseType {
     protected parse(v: number | undefined): Exception | void {
         const error = new Exception()
         const type = typeof v
-        const { lt, lte, gt, gte, float, int } = this._options
+        const { lt, lte, gt, gte, float, int, validate } = this._options
         if (type === 'undefined') {
             if (this._options.required) error.addIssue({ path: 'value', expected: 'number', received: type })
         } else if (type === 'number') {
-            if (lt !== undefined && (v as number) >= lt) error.addIssue({ path: 'value', expected: `<${lt}`, received: v as number })
-            else if (lte !== undefined && (v as number) > lte) error.addIssue({ path: 'value', expected: `<=${lte}`, received: v as number })
-            else if (gt !== undefined && (v as number) <= gt) error.addIssue({ path: 'value', expected: `>${gt}`, received: v as number })
-            else if (gte !== undefined && (v as number) < gte) error.addIssue({ path: 'value', expected: `>=${gte}`, received: v as number })
-            else if (float && !isFloat(v as number)) error.addIssue({ path: 'value', expected: `float`, received: 'integer' })
-            else if (int && !isInt(v as number)) error.addIssue({ path: 'value', expected: `integer`, received: 'float' })
+            if (lt !== undefined && v! >= lt) error.addIssue({ path: 'value', expected: `<${lt}`, received: v })
+            if (lte !== undefined && v! > lte) error.addIssue({ path: 'value', expected: `<=${lte}`, received: v })
+            if (gt !== undefined && v! <= gt) error.addIssue({ path: 'value', expected: `>${gt}`, received: v })
+            if (gte !== undefined && v! < gte) error.addIssue({ path: 'value', expected: `>=${gte}`, received: v })
+
+            if (float && !isFloat(v!)) error.addIssue({ path: 'value', expected: `float`, received: 'integer' })
+            else if (int && !isInt(v!)) error.addIssue({ path: 'value', expected: `integer`, received: 'float' })
+
+            if (validate) {
+                const message = validate(v)
+                if (message) error.addIssue({ path: 'value', message })
+            }
         } else error.addIssue({ path: 'value', expected: 'number', received: type })
         if (error.hasIssues()) return error
     }
